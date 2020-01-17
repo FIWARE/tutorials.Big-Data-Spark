c7353c5886f2a08bfe56ff9d9a517569014f104c
Javier <javierlopezjimeno@gmail.com>
2020-01-17 11:24:38 +0100

    Example 1

---
+++ job/src/main/scala/org/fiware/cosmos/orion/spark/connector/tutorial/example1/Example1.scala
@@ -0,0 +1,35 @@
+package org.fiware.cosmos.orion.spark.connector.tutorial.example1
+
+
+import org.apache.spark._
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.fiware.cosmos.orion.spark.connector._
+/**
+  * Example1 Orion Spark Tutorial
+  * @author @Javierlj
+  */
+object Example1{
+
+  def main(args: Array[String]): Unit = {
+
+    val conf = new SparkConf().setMaster("local[4]").setAppName("Example 1")
+    val ssc = new StreamingContext(conf, Seconds(10))
+    // Create Orion Source. Receive notifications on port 9001
+    val eventStream = ssc.receiverStream(new OrionReceiver(9001))
+
+    // Process event stream
+    eventStream
+      .flatMap(event => event.entities)
+      .map(ent => {
+        new Sensor(ent.`type`)
+      })
+      .countByValue()
+      .window(Seconds(10))
+      .print()
+
+
+    ssc.start()
+    ssc.awaitTermination()
+  }
+  case class Sensor(device: String)
+}
